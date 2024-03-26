9b8683f4a2032a100d0a55dbee7c571f53ffc5af
zhangzihengsayou <161273074+zhangzihengsayou@users.noreply.github.com>
2024-03-26 13:54:42 +0800

    Create MusicConcertEvent.java

---
+++ src/MusicConcertEvent.java
@@ -0,0 +1,27 @@
+public class MusicConcertEvent  {
+    private boolean merchandiseRequired;
+    private double merchandiseCost;
+
+
+    public MusicConcertEvent(boolean merchandiseRequired, double merchandiseCost) {
+        this.merchandiseRequired = merchandiseRequired;
+        this.merchandiseCost = merchandiseCost;
+    }
+
+    public double calculateEventCost() {
+        double eventCost = 0.0;
+
+        if (merchandiseRequired) {
+            eventCost += merchandiseCost;
+        }
+
+        return eventCost;
+    }
+    @Override
+    public String toString() {
+        return "Music Concert Event\n" +
+                "Merchandise Required: " + merchandiseRequired + "\n" +
+                "Merchandise Cost: $" + merchandiseCost + "\n" +
+                "Total Event Cost: $" + calculateEventCost();
+    }
+}
